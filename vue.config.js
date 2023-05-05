7b68d27b2e881c877deb2dbcd23ea4bf8ff3bf3e
Mathieu Cauquil <mathieu.cauquil@bleu122.com>
2023-05-06 01:43:37 +0200

    modify colors of pwa

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: "The Gavel's Call",
+    themeColor: 'red',
+    msTileColor: '#000000'
+  }
+}
