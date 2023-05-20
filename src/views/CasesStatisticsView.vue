67e5607240a1afbaf09873f9443cdd141495a42e
Mathieu Cauquil <mathieu.cauquil@bleu122.com>
2023-05-20 21:22:30 +0200

    created cases verdict charge

--- src/views/CasesStatisticsView.vue
+++ src/views/CasesStatisticsView.vue
@@ -4,23 +4,48 @@
             <i class="ti ti-arrow-back"/>
         </b-button>
         <b-row align-h="center" align-v="center" class="table-history-cases">
-            <div class="col-12">
+            <div v-if="casesGuilty === 0" class="col-12">
                 <h1>No data</h1>
+            </div>
+            <div v-else class="col-auto">
+                <apexchart type="pie" width="450" :options="chartOptions" :series="series"></apexchart>
             </div>
         </b-row>
     </div>
 </template>
 <script>
 import {mapActions, mapState} from "vuex";
-
+import VueApexCharts from 'vue-apexcharts';
 export default {
     name: 'CasesStatisticsView',
     data() {
         return {
             screenWidth: 0,
             casesGuilty: 0,
-            casesNotGuilty: 0
+            casesNotGuilty: 0,
+            series: [],
+            chartOptions: {
+                chart: {
+                    width: 380,
+                    type: 'pie',
+                },
+                labels: ['Cases Guilty', 'Cases Not Guilty'],
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 200
+                        },
+                        legend: {
+                            position: 'bottom'
+                        }
+                    }
+                }]
+            },
         }
+    },
+    components: {
+        apexchart: VueApexCharts,
     },
     mounted() {
         this.updateScreenWidth();
@@ -41,7 +66,10 @@ export default {
                 }
             }
             result = ((result * 100) / this.historicalCases.length);
-            console.log("result: ", result.toFixed(2));
+            this.casesGuilty = result;
+            this.casesNotGuilty = 100 - this.casesGuilty;
+            console.log("result: ", result);
+            this.series.push(this.casesGuilty, this.casesNotGuilty);
         },
         onScreenResize() {
             window.addEventListener("resize", () => {
