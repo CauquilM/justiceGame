62cb97e648bd671d5ded7c383e555d3e7e35e0d6
Mathieu Cauquil <mathieu.cauquil@bleu122.com>
2023-05-26 13:57:40 +0200

    work on penal code page

--- src/views/PenalCodeView.vue
+++ src/views/PenalCodeView.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-
+  <div class="penal-code-view-container">
+      <h1>Penal code page</h1>
+      <p>Click on an offense to see details</p>
+      <div class="mt-5 penal-code-view-div">
+          <div v-for="offense in offenses" :key="offense.offense_charge">
+              <p>{{ offense.offense_charge }}</p>
+          </div>
+      </div>
   </div>
 </template>
 <script>
