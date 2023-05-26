a29de8d00fff747e6e1783b817fcdba52f3d0f99
Mathieu Cauquil <mathieu.cauquil@bleu122.com>
2023-05-26 13:19:02 +0200

    update offences

---
+++ src/views/PenalCodeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+import offenses from "@/data_cases/penal_code/offenses.json"
+  export default {
+      name: 'PenalCodeView',
+      data(){
+          return {
+              offenses: undefined
+          }
+      },
+      created() {
+          this.offenses = offenses;
+          console.log(offenses)
+      }
+  }
+</script>
